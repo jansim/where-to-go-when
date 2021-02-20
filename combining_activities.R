ecd58a2c65bbb2360d4cdd961bc8c8fa0762a894
SophBr <sophie.breunig@yahoo.de>
2021-02-21 00:27:23 +0100

    combining data now including atlas obscura data and now excluding "go" category

--- combining_activities.R
+++ combining_activities.R
@@ -1,5 +1,8 @@
 library(tidyverse)
+library(rjson)
+library(jsonlite)
 
+# loading datasets
 wiki <- read_csv(
   "data/wikivoyage-listings-en-latest.csv",
   col_types = cols(
@@ -19,6 +22,9 @@ wiki <- read_csv(
 
 mp_routes <- read_csv("data/mp_routes.csv")
 
+ao <- read_json("data/ao.json", simplifyVector = TRUE)
+
+# filtering activities
 wiki <- wiki %>%
   filter(!is.na(lat), !is.na(lon)) %>%
   filter(abs(lon) < 180, abs(lat) < 90)
@@ -34,7 +40,7 @@ wiki %>%
   count(type)
 
 wiki_final <- wiki %>%
-  filter(type %in% c("see", "do", "camping", "city", "go")) %>%
+  filter(type %in% c("see", "do", "camping", "city")) %>%
   select(
     cat = type,
     lat, lon, title, phone, url
@@ -46,8 +52,12 @@ wiki_final %>%
 ggplot(wiki_final, aes(x = lon, y = lat, color = cat)) +
   geom_point()
 
+#making columns that make sense to show as info
+wiki_final$url[is.na(wiki_final$url)] <- " "
+wiki_final$phone[is.na(wiki_final$phone)] <- " "
+
 wiki_final$description <- paste(wiki_final$url,
-                                "/n",
+                                "\n",
                                 wiki_final$phone)
 
 
@@ -75,11 +85,15 @@ final_mp$title <- final_mp$Route
 
 final_mp <- final_mp[,c(4,5,6,7,8)]
 
+#adding atlas obscura colums
+ao$cat <- "ao"
+ao$title <- ao$id
+ao$description <- " "
+ao$lon <- ao$lng
 
-combinedData <- rbind(wiki_final[,c(1,2,3,4,7)], final_mp)
+#combining data
+combinedData <- rbind(wiki_final[,c(1,2,3,4,7)], final_mp, ao[,c(2,4,5,6,7)])
 
-
-#jsonlite::write_json(combinedData, path = "data_cleaned/CombinedActivities.json")
-#jsonlite::write_json(wiki_final, path = "data_cleaned/wiki_voyage.json")
+#saving data
 write_csv(combinedData, "web/static/data_cleaned/combined_activities.csv")
 
